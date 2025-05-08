58f1160a6183e4d1b7fa6a3c92d22775f1859b07
Luis Felipe <luis.felipe.lopes1275@gmail.com>
2025-05-07 22:28:33 -0400

    Criado diretório das rotas e um arquivo de rota para exemplificação da explicação da aula

---
+++ src/routes/eventoRoutes.js
@@ -0,0 +1,14 @@
+// src/routes/eventoRoutes.js
+
+import express from "express";
+
+// import EventoController from '../controllers/EventoController.js';
+import { asyncWrapper } from "../utils/helpers";
+
+
+
+const router = express.Router();
+
+// const usuarioController = new UsuarioController();  // Instância da classe
+
+router
