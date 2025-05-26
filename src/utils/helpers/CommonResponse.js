aa7a7171c931bfae0019435835eaaa323dc7e6fe
luis felipe <luis.felipe.lopes1275@gmail.com>
2025-05-26 15:28:22 -0400

    Implementando testes do diretório utils

--- src/utils/helpers/CommonResponse.js
+++ src/utils/helpers/CommonResponse.js
@@ -3,14 +3,18 @@
 import StatusService from './StatusService.js';
 
 class CommonResponse {
-    constructor(message, data = null, errors = []) {
+    constructor(message, data = null, errors = [], error = false, code = null) {
         this.message = message;
         this.data = data;
         this.errors = errors;
+        this.error = error;
+        this.code = code;
     }
 
     toJSON() {
         return {
+            error: this.error,
+            code: this.code,
             message: this.message,
             data: this.data,
             errors: this.errors
@@ -19,13 +23,13 @@ class CommonResponse {
 
     static success(res, data, code = 200, message = null) {
         const statusMessage = message || StatusService.getHttpCodeMessage(code);
-        const response = new CommonResponse(statusMessage, data, []);
+        const response = new CommonResponse(statusMessage, data, [], false, code);
         return res.status(code).json(response);
     }
 
     static error(res, code, errorType, field = null, errors = [], customMessage = null) {
         const errorMessage = customMessage || StatusService.getErrorMessage(errorType, field);
-        const response = new CommonResponse(errorMessage, null, errors);
+        const response = new CommonResponse(errorMessage, null, errors, true, code);
         return res.status(code).json(response);
     }
 
@@ -35,7 +39,7 @@ class CommonResponse {
 
     static serverError(res, message = null) {
         const errorMessage = message || StatusService.getErrorMessage('serverError');
-        const response = new CommonResponse(errorMessage, null, []);
+        const response = new CommonResponse(errorMessage, null, [], true, 500);
         return res.status(500).json(response);
     }
 
@@ -50,10 +54,12 @@ class CommonResponse {
         return {
             type: "object",
             properties: {
+                error: { type: "boolean", example: false },
+                code: { type: "integer", example: 200 },
+                message: { type: "string", example: messageExample },
                 data: schemaRef
                     ? { $ref: schemaRef }
                     : { type: "array", items: {}, example: [] },
-                message: { type: "string", example: messageExample },
                 errors: { type: "array", example: [] }
             }
         };
